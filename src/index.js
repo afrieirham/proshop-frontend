efc44a80adbfcb2f4878f95addc924693caa6c74
afrie <itsafrie@gmail.com>
2020-10-01 17:32:15 +0800

    Setup axios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:5000'
 
 ReactDOM.render(
   <React.StrictMode>
