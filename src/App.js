9b6ae14e5798325449569c1b9a562d6fbab75892
afrie <itsafrie@gmail.com>
2020-10-01 15:16:42 +0800

    Theme initial setup with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <>
-      <p>Welcome to ProShop</p>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
