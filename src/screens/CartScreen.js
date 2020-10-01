615575344cfc3a815b2d6712d827655479e8d883
afrie <itsafrie@gmail.com>
2020-10-02 00:25:06 +0800

    Implement addToCart action and store in localstorage

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart } from '../redux/actions/cartActions'
 
-function CartScreen() {
+import Message from '../components/Message'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+
+function CartScreen({ match, location, history }) {
+  const productId = match.params.id
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const dispatch = useDispatch()
+
+  const { cartItems } = useSelector(({ cart }) => cart)
+
+  console.log(cartItems)
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+
   return <div>cart</div>
 }
 
