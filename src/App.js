e9b613511dccef9193b24d4fc5c769089b7f7993
afrie <itsafrie@gmail.com>
2020-10-04 00:12:38 +0800

    Add place order screen

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
             <Route path='/login' component={LoginScreen} />
             <Route path='/shipping' component={ShippingScreen} />
             <Route path='/payment' component={PaymentScreen} />
+            <Route path='/place-order' component={PlaceOrderScreen} />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
           </Switch>
