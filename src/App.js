12d3516d00a3386a0efb4cbee433f28d988d3260
afrie <itsafrie@gmail.com>
2020-10-03 21:01:50 +0800

    Add view user profile screen

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Container>
           <Switch>
             <Route exact path='/' component={HomeScreen} />
+            <Route path='/profile' component={ProfileScreen} />
             <Route path='/register' component={RegisterScreen} />
             <Route path='/login' component={LoginScreen} />
             <Route path='/product/:id' component={ProductScreen} />
