09b64a06f78fe3247a350e08eb0a49af7448e2d8
afrie <itsafrie@gmail.com>
2020-10-06 00:19:21 +0800

    Reset searchbox if pathname change

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 
 import { Form, Button } from 'react-bootstrap'
 
 function SearchBox() {
   const history = useHistory()
+  const location = useLocation()
 
   const [keyword, setKeyword] = useState('')
   const submitHandler = (e) => {
@@ -15,6 +16,13 @@ function SearchBox() {
       history.push('/')
     }
   }
+
+  useEffect(() => {
+    if (!location.pathname.includes('search')) {
+      setKeyword('')
+    }
+  }, [location.pathname])
+
   return (
     <Form onSubmit={submitHandler} inline>
       <Form.Control
@@ -22,6 +30,7 @@ function SearchBox() {
         name='q'
         placeholder='Search product...'
         className='mr-sm-2 ml-sm-5'
+        value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
       ></Form.Control>
       <Button type='submit' variant='outline-light' className='p-2'>
