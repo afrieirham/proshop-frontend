cbef1531ea738c79dc5ecbe07e94960c2dab9e86
Afrie Irham <itsafrie@gmail.com>
2021-01-21 01:59:04 +0800

    Add PrivateRoute component to handle private route

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { Route, useHistory, useLocation } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../redux/actions/userActions'
+
+function ProtectedRoute({ admin, ...rest }) {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const location = useLocation()
+  const { userInfo } = useSelector((state) => state.userLogin)
+
+  useEffect(() => {
+    // Kick user to login screen if not logged in
+    if (!userInfo) {
+      dispatch(logout())
+      return history.push('/login')
+    }
+
+    // Kick user to homescreen if not admin
+    if (admin && !userInfo.isAdmin) {
+      return history.push('/')
+    }
+  }, [dispatch, location, history, userInfo, admin])
+
+  return <Route {...rest} />
+}
+
+export default ProtectedRoute
