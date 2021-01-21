3f0f9d6a2af85f900d3ad666e6c94b8b0936c732
Afrie Irham <itsafrie@gmail.com>
2021-01-22 04:11:07 +0800

    Remove unused variable

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -13,11 +13,9 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
-  USER_DETAILS_RESET,
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
-  USER_LIST_RESET,
   USER_DELETE_REQUEST,
   USER_DELETE_SUCCESS,
   USER_DELETE_FAIL,
@@ -38,7 +36,6 @@ import {
   USER_SHOW_CHILD_FAIL,
 } from '../types/userTypes'
 
-import { ORDER_LIST_MY_RESET } from '../types/orderTypes'
 
 export const login = (email, password) => async (dispatch) => {
   try {
